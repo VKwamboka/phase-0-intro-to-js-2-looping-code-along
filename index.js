ec6cf2d6a5521ae16af3c17181c203acb915228e
verah <verahnyagaka101@gmail.com>
2022-06-03 12:01:10 +0300

    commit tree dc98f8445d9d0da0fdc59d2ee496e25e0bc8bc69

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(name){
+    for ( let count = 0;count < name.length; count++) {
+    messages.push (`Thank you, ${name[count]}, for the wonderful surprise gift!`);
+    }   
+    return messages;  
+}
+
+function  countDown(number){
+    //let  number;
+    for (let i = number; i>=0; i--){
+        console.log (i);
+
+    }
+    return number;
+}
